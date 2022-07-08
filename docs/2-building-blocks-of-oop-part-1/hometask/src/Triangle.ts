abd65a72780a9f12191c292697c5b7fd1be273f2
vladislavrepkin <vladislav_repkin@epam.com>
2022-07-08 14:54:40 +0300

    oop part 1 triangle enum

--- docs/2-building-blocks-of-oop-part-1/hometask/src/Triangle.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Triangle.ts
@@ -1,26 +1,37 @@
 import { IShape, Shape } from './Shape';
 import { Point } from './Point';
 
-interface ITriangle extends IShape {
-  getType: () => ('equilateral triangle' | 'isosceles triangle' | 'scalene triangle');
+enum TriangleTypes {
+  equilateral,
+  isosceles,
+  scalene
 }
 
-export class Triangle extends Shape implements  ITriangle {
+const triangleTypesMap = {
+  [TriangleTypes.equilateral]: 'equilateral',
+  [TriangleTypes.isosceles]: 'isosceles',
+  [TriangleTypes.scalene]: 'scalene',
+}
+
+export class Triangle extends Shape {
   constructor (private p1: Point, private p2: Point, private p3: Point, color?: string, filled?: boolean) {
     super([p1, p2, p3], color, filled);
+  }
+  getType (): string {
+    return `${triangleTypesMap[this.defineType()]} triangle`;
   }
   toString = () => {
     return `Triangle[v1=(${this.p1.x}, ${this.p1.y}),v2=(${this.p2.x}, ${this.p2.y}),v3=(${this.p3.x}, ${this.p3.y})]`
   }
-  getType = () => {
+  private defineType = (): TriangleTypes => {
     const sides = [this.p1, this.p2, this.p3]
       .map((cur, index, arr) =>
         cur.distance(arr[index === arr.length - 1 ? 0 : index + 1]));
     const sameSidesCount = sides.length + 1 - new Set(sides).size;
     switch (sameSidesCount) {
-      case 3: return 'equilateral triangle';
-      case 2: return 'isosceles triangle';
-      default: return 'scalene triangle';
+      case 3: return TriangleTypes.equilateral;
+      case 2: return TriangleTypes.isosceles;
+      default: return TriangleTypes.scalene;
     }
   }
 }

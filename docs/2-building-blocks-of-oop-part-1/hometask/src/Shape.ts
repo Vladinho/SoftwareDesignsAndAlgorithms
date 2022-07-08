f5b53404cc866782cd7dd0d4c536039309b6b4db
vladislavrepkin <vladislav_repkin@epam.com>
2022-07-08 14:48:37 +0300

    oop part 1 interfaces are fixed

--- docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
@@ -1,6 +1,12 @@
 import { Point } from './Point';
 
-export abstract class Shape {
+export interface IShape {
+    getType: () => string;
+    toString: () => string;
+    getPerimeter: () => number;
+}
+
+export abstract class Shape implements IShape {
     private MIN_POINTS_NUMBER = 3;
     constructor (
       private points: Point[],
@@ -11,14 +17,14 @@ export abstract class Shape {
             throw new Error(`Min points number is ${this.MIN_POINTS_NUMBER}, but you have only ${this.points.length}.`);
         }
     }
-    abstract getType(): string;
+    abstract getType();
     toString = () => {
         const points = this.points.reduce((acc, cur, currentIndex) => {
             return currentIndex ? `${acc}, ${cur.toString()}` : cur.toString();
         }, '')
         return `A Shape with color of ${this.color} and ${this.filled ? '' : 'not '}filled. Points: ${points}.`
     }
-    getPerimeter = (): number => {
+    getPerimeter = () => {
         return this.points.reduce((acc, cur, currentIndex) => {
             return acc + (currentIndex ? cur.distance(this.points[currentIndex - 1]) : cur.distance(this.points[this.points.length - 1]));
         }, 0)
--- docs/2-building-blocks-of-oop-part-1/hometask/src/Triangle.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Triangle.ts
@@ -1,25 +1,26 @@
-import { Shape } from './Shape';
+import { IShape, Shape } from './Shape';
 import { Point } from './Point';
 
-export class Triangle extends Shape {
+interface ITriangle extends IShape {
+  getType: () => ('equilateral triangle' | 'isosceles triangle' | 'scalene triangle');
+}
+
+export class Triangle extends Shape implements  ITriangle {
   constructor (private p1: Point, private p2: Point, private p3: Point, color?: string, filled?: boolean) {
     super([p1, p2, p3], color, filled);
   }
-  getType (): string {
-    return `${this.defineType()} triangle`;
-  }
-  toString = (): string => {
+  toString = () => {
     return `Triangle[v1=(${this.p1.x}, ${this.p1.y}),v2=(${this.p2.x}, ${this.p2.y}),v3=(${this.p3.x}, ${this.p3.y})]`
   }
-  private defineType = (): string => {
+  getType = () => {
     const sides = [this.p1, this.p2, this.p3]
       .map((cur, index, arr) =>
         cur.distance(arr[index === arr.length - 1 ? 0 : index + 1]));
     const sameSidesCount = sides.length + 1 - new Set(sides).size;
     switch (sameSidesCount) {
-      case 3: return 'equilateral';
-      case 2: return 'isosceles';
-      default: return 'scalene';
+      case 3: return 'equilateral triangle';
+      case 2: return 'isosceles triangle';
+      default: return 'scalene triangle';
     }
   }
 }

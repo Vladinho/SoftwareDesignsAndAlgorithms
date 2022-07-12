7eb183df968c86f4c3fa39832b284c5ba40ddf0b
vladislavrepkin <vladislav_repkin@epam.com>
2022-07-12 22:28:50 +0300

    oop part 2

--- docs/3-building-blocks-of-oop-part-2/hometask/src/Consumable.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Consumable.ts
@@ -1,1 +1,31 @@
-// your code goes here
+import { IItemProps, Item } from './Item';
+
+interface IConsumable {
+  use: () => string;
+}
+
+interface IConsumableProps extends IItemProps {
+  spoiled: boolean
+}
+
+export class Consumable extends Item implements IConsumable {
+  private isSpoiled: boolean
+  private isConsumed: boolean
+
+  constructor ({name, value, weight, spoiled}: IConsumableProps) {
+    super({name, value, weight});
+    this.isSpoiled = spoiled;
+  }
+
+  public use = () => {
+    if (this.isConsumed) {
+      return `There is nothing left of the ${this.name} to consume.`;
+    }
+    if (this.isSpoiled) {
+      return `${this.eat()}\nYou feel sick.`
+    }
+    return this.eat();
+  };
+  private eat = () => `You eat the ${this.name}.`;
+  private setConsumed = (consumed: boolean) => this.isConsumed = consumed;
+}
--- docs/3-building-blocks-of-oop-part-2/hometask/src/Item.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Item.ts
@@ -2,10 +2,47 @@ import { Comparable } from './Comparable';
 
 let id = 0;
 
-export abstract class Item implements Comparable<Item> {
-    public compareTo(other: Item): number {
-        // your code goes here
+export interface IItemProps {
+    name: string;
+    value: number;
+    weight: number;
+}
+
+interface IItem {
+    toString: () => string;
+}
+
+export abstract class Item implements Comparable<Item>, IItem {
+    protected readonly id;
+    protected value;
+    protected name;
+    protected weight;
+    protected constructor ({ name, value, weight }: IItemProps) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+        this.weight = weight;
+        id++;
     }
 
-    // your code goes here
+    static reset = () => id = 0;
+    public compareTo({ weight }: Item): number {
+        if (this.weight > weight) {
+            return -1;
+        } else if (this.weight === weight) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    public toString() {
+        return `${this.name} − Value: ${this.value}, Weight: ${this.weight}`;
+    }
+    private getId = () => this.id;
+    private getValue = () => this.value;
+    private getName = () => this.name;
+    private getWeight = () => this.weight;
+    private setValue = (value: number) => this.value = value;
+    private setName = (name: string) => this.name = name;
+    private setWeight = (weight: number) => this.weight = weight;
 }
